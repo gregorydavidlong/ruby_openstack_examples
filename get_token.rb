e7999052f34551094710e58d3d3e12db56a2d86f
Gregory Long <gregorydavidlong@gmail.com>
2012-03-27 11:43:00 +1100

    Improved get_token example

--- get_token.rb
+++ get_token.rb
@@ -5,12 +5,14 @@
 
 require 'net/http'
 require 'json'
-require './credentials'
+require './credentials' # You will need to create this file. See README.
 
 URL = URI('http://keystone.rc.nectar.org.au:35357/v2.0/tokens')
 
-# Class for generating JSON
+
 class TokenRequest
+
+  # Method for generating JSON for token request
   def self.to_json(user, password, tenant_id)
     JSON.generate (
       {
@@ -24,19 +26,29 @@ class TokenRequest
       }
     })
   end
+
+  # Call this to get your auth token
+  def self.get_token(user, password, tenant_id)
+
+    # make a connection
+    token_id = Net::HTTP.start(URL.host, URL.port) do |http|
+      request = Net::HTTP::Post.new URL.path
+    
+      # set the content type
+      request.content_type = 'application/json'
+    
+      # set the body of the request
+      request.body = TokenRequest.to_json user, password, tenant_id
+      
+      raw_response = http.request request
+      json_response = JSON.parse raw_response.body
+    
+      token_id = json_response['access']['token']['id']
+      token_id
+    end
+    token_id
+  end
 end
 
-
-# Try using JSON
-Net::HTTP.start(URL.host, URL.port) do |http|
-  request = Net::HTTP::Post.new URL.path
-
-  # set the content type
-  request.content_type = 'application/json'
-
-  # set the body of the request
-  request.body = TokenRequest.to_json $USER, $PASSWORD, $TENANT_ID
-  
-  response = http.request request
-  print response.body
-end
+# Example usage
+print TokenRequest.get_token($USER, $PASSWORD, $TENANT_ID) + "\n"
