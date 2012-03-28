3ba1692bfb5de02ba27a3200c702dad2bee0d82d
Gregory Long <gregorydavidlong@gmail.com>
2012-03-29 10:21:08 +1100

    Added examples for adding and viewing metadata

---
+++ set_server_metadata.rb
@@ -0,0 +1,40 @@
+# Set server metadata
+require './get_token'
+require 'net/http'
+require './credentials' # You will need to create this file. See README.
+
+server_id = '2620'
+
+url = URI('http://nova.rc.nectar.org.au:8774/v1.1/' + $TENANT_ID + '/servers/' + server_id + '/metadata')
+
+
+class ServerSetMetadata
+  def self.to_json(key, value)
+    JSON.generate (
+    {
+      'metadata' => 
+      {
+        key => value
+      }
+    })
+  end
+end
+
+
+
+Net::HTTP.start(url.host, url.port) do |http|
+      request = Net::HTTP::Put.new url.path
+    
+      # set the content type
+      request.content_type = 'application/json'
+      request['X-Auth-Token'] = TokenRequest.get_token($USER, $PASSWORD, $TENANT_ID)
+      request['Accept'] = 'application/json'
+    
+      # set the body of the request
+      request.body = ServerSetMetadata.to_json "a metadata key", "a metadata value"
+      
+      raw_response = http.request request
+   
+      print raw_response.body
+
+    end
